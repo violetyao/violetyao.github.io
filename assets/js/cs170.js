f3b4fa9e571728451f6feb308193c0c7c7c18296
Violet Yao <violetyao@berkeley.edu>
2019-03-25 01:43:32 -0600

    added resume

---
+++ assets/js/cs170.js
@@ -0,0 +1,6 @@
+$(function () {
+    let div = $('#dual-eg');
+    $('#show-eg').click(function () {
+        div.fadeToggle(1000);
+    });
+});
---
+++ assets/js/cs188.js
@@ -0,0 +1,13 @@
+$(function () {
+    let div = $('#forward-checking-fail-eg');
+    $('#show-forward-checking-fail-eg').click(function () {
+        div.fadeToggle(1000);
+    });
+});
+
+$(function () {
+    let div = $('#entailment-eg');
+    $('#show-entailment-eg').click(function () {
+        div.fadeToggle(1000);
+    });
+});
